993db5fdf99c016f261fe9251ddc7d5d33805dc1
Alejandro Junqui <ajunquit@gmail.com>
2025-04-06 22:09:57 -0500

    add AdditionOperationNotImplemented

---
+++ Addition.Service/Domain/Exceptions/AdditionOperationNotImplemented.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace Addition.Service.Domain.Exceptions
+{
+    public class AdditionOperationNotImplemented : Exception
+    {
+        public string Code { get; set; }
+
+        public AdditionOperationNotImplemented() : base() { }
+
+        public AdditionOperationNotImplemented(string message) : base(message, null) { }
+
+        public AdditionOperationNotImplemented(string code, string message) : base(message, null)
+        {
+            Code = code;
+        }
+
+        public AdditionOperationNotImplemented(string message, Exception innerException) : base(message, innerException) { }
+
+        protected AdditionOperationNotImplemented(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
